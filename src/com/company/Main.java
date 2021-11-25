bfecc9f24f75ce0cc5ac8ece95076fbeff7b4a50
maxrule702 <maximoo2004@gmail.com>
2021-11-25 21:46:02 +0000

    project raffle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,32 +8,53 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        int day = 1;
-//        switch (day) {
-//            case 1:
-//                System.out.println("Monday");
-//                break;
+    int placeholder = 0;
 
-        int generatedNumber = 0;
-                System.out.println("would you like to check a ticket or purchase one?");
-        Scanner input = new Scanner(System.in);
-        String userInput = input.next();
-        if (userInput.equals("check")) {
-            System.out.println("please enter your number");
-            int numberCheck = input.nextInt();
-            checkPrime(numberCheck);
-        }
-        if (userInput.equals("buy")) {
-            System.out.println("generating number...");
-            for (int i = 0; i <= 0; i++) {
-                int x = 1 + (int) (Math.random() * 100);
-            generatedNumber =  x;
+        while (placeholder !=1) {
+            int generatedNumber = 0;
+            System.out.println("would you like to check a ticket or purchase one?");
+            Scanner input = new Scanner(System.in);
+            String userInput = input.next();
+            if (userInput.equals("check")) {
+                System.out.println("please enter your number");
+                int numberCheck = input.nextInt();
+                checkPrime(numberCheck);
             }
-            System.out.println("your number is "+generatedNumber);
-            System.out.println("checking number...");
-            checkPrime(generatedNumber);
+            if (userInput.equals("buy")) {
+                System.out.println("generating number...");
+                for (int i = 0; i <= 0; i++) {
+                    int x = 1 + (int) (Math.random() * 100);
+                    generatedNumber = x;
+                }
+                System.out.println("your number is " + generatedNumber);
+                System.out.println("checking number...");
+                checkPrime(generatedNumber);
+                System.out.println("would you like to check or buy a new ticket?");
+                String again = input.next();
+                if (again.equals("yes")) {
+                    int restartCheck = 1;
+                    switch (restartCheck) {
+                        case 1:
+                            System.out.println("restarting...");
+                    }
+                }
+                   else{
+                       int restartCheck =2;
+                       switch (restartCheck) {
+                        case 2:
+                            System.out.println("goodbye");
+                            placeholder++;
+                            break;
+
+
+                            }
+                        }
+                    }
+
+               }
+            }
         }
-    }
-}
+
+
 
 
